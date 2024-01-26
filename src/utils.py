c7b2d2f88e26d849ce107cc36c379e34441eb2b9
Nikita Smykov <132088678+Apicqq@users.noreply.github.com>
2024-01-26 10:36:32 +0300

    refactor: simplify code, add constants instead of literals, add get_soup function to simplify main logic

--- src/utils.py
+++ src/utils.py
@@ -1,38 +1,43 @@
 # Название этого файла — антипаттерн. Логичнее его было бы назвать, например,
 # services или helpers.
+from typing import Optional, Union
 
-import logging
-from typing import Optional
-
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from requests import RequestException
-from requests_cache import CachedSession
+from requests_cache import CachedSession, Response
 
+from constants import Literals
 from exceptions import ParserFindTagException
 
 
-def get_response(session: CachedSession, url: str):
+def get_response(
+        session: CachedSession, url: str, encoding: str = 'utf-8'
+) -> Response:
     """
     Получить содержимое веб-страницы по указанному URL.
 
     :param session: CachedSession - сессия, используемая для запроса.
     :param url: URL веб-страницы.
+    :param encoding: Кодировка веб-страницы, по умолчанию — utf-8.
     :returns: Объект ответа HTTP.
+    :raises ConnectionError: Если произошла ошибка подключения.
     """
     try:
         response = session.get(url)
-        response.encoding = 'utf-8'
+        response.encoding = encoding
         return response
-    except RequestException:
-        logging.exception(f'Возникла ошибка при загрузке страницы {url}',
-                          stack_info=True)
+    except RequestException as error:
+        raise ConnectionError(
+            Literals.REQUEST_EXCEPTION.format(url)
+        ) from error
 
 
-def find_tag(soup: BeautifulSoup, tag: str, attrs: Optional[dict] = None):
+def find_tag(soup: Union[BeautifulSoup, Tag], tag: str,
+             attrs: Optional[dict] = None):
     """
     Находит указанный тег в объекте BeautifulSoup.
 
-    :param soup: Объект BeautifulSoup, представляющий HTML-документ.
+    :param soup: Объект BeautifulSoup или Tag, представляющий HTML-документ.
     :param tag: Имя тега, который нужно найти.
     :param attrs: Дополнительные атрибуты для поиска тега (необязательно).
     :return: Найденный тег.
@@ -40,7 +45,17 @@ def find_tag(soup: BeautifulSoup, tag: str, attrs: Optional[dict] = None):
     """
     searched_tag = soup.find(tag, attrs=(attrs or {}))
     if not searched_tag:
-        error_msg = f'Не найден тег {tag} {attrs}'
-        logging.error(error_msg, stack_info=True)
-        raise ParserFindTagException(error_msg)
+        raise ParserFindTagException(Literals.TAG_NOT_FOUND.format(tag, attrs))
     return searched_tag
+
+
+def get_soup(session: CachedSession, url: str) -> BeautifulSoup:
+    """
+    Преобразует объект Response в BeautifulSoup.
+
+    :param session: Объект Response, представляющий HTML-документ.
+    :param url: URL веб-страницы.
+    :return: Объект BeautifulSoup, представляющий HTML-документ.
+    """
+    response = get_response(session, url)
+    return BeautifulSoup(response.text, 'lxml')
